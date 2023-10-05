67dff7e76ca9da73004256f99f86690fa5377014
jainaga <francisco.reyesja@sansano.usm.cl>
2023-10-04 22:03:20 -0300

    Primer cambio note

--- fullstack/src/App.js
+++ fullstack/src/App.js
@@ -2,6 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const autos = [
+    {
+      name: "Mazda",
+      model: "cx-3"
+    }
+    ,
+    {
+      name: "Mazda",
+      model: "bt-50"
+    }
+  ]
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +20,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <li>
+          El auto que me gusta es un {autos[0].name} {autos[0].model}
+        </li>
         <a
           className="App-link"
           href="https://reactjs.org"
