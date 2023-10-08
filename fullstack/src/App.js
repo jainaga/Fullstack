fd3b40b4ca3d0f9de7c57c70c1dfd51aebe22f26
jainaga <francisco.reyesja@sansano.usm.cl>
2023-10-07 22:57:13 -0300

    Listo los ejercicios de contador de votos

--- fullstack/src/App.js
+++ fullstack/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const Statistics = ({counts}) => {
+const Statistics = ({counts,estadistica,nombre_valor}) => {
 
     const porcentaje = () => {
     const votos = [...counts];
@@ -14,46 +14,91 @@ const Statistics = ({counts}) => {
     return positivos;
   }
 
+  
+
   return (
     <div>
-      <ul>average {porcentaje()}</ul>
-      <ul>positive {positive()}%</ul>
+      {estadistica === "good" && nombre_valor === "nombre" && <ul>{estadistica}</ul>}
+      {estadistica === "good" && nombre_valor === "valor" && <ul>{counts[0]}</ul>}
+      {estadistica === "neutral" && nombre_valor === "valor" && <ul>{counts[1]}</ul>}
+      {estadistica === "bad" && nombre_valor === "valor" && <ul>{counts[2]}</ul>}
+      {estadistica === "total" && nombre_valor === "valor" && <ul>{counts[0]+counts[1]+counts[2]}</ul>}
+      {estadistica === "average" && nombre_valor === "valor" && <ul>{porcentaje()}</ul>}
+      {estadistica === "positive" && nombre_valor === "valor" && <ul>{positive()}%</ul>}
+      {estadistica === "positive" && nombre_valor === "nombre" && <ul>{estadistica}</ul>}
+      {estadistica === "average" && nombre_valor === "nombre" && <ul>{estadistica}</ul>}
+      {estadistica === "total" && nombre_valor === "nombre" && <ul>{estadistica}</ul>}
+      {estadistica === "bad" && nombre_valor === "nombre" && <ul>{estadistica}</ul>}
+      {estadistica === "neutral" && nombre_valor === "nombre" && <ul>{estadistica}</ul>}
     </div>
   );
 };
 
 
-function App() {
- 
-  const [counts, setCounts] = useState([0, 0, 0]);
-  const [isVisible, setIsVisible] = useState(false);
-
+const Button = ({counts, setCounts, setIsVisible, text}) => {
   const handleFeedback = (pos) => {
     const newCounts = [...counts];
     newCounts[pos] += 1;
     setCounts(newCounts);
     setIsVisible(true);
   }
- 
+
+  return (
+    <div>
+      {text === "good" && <button onClick={() => handleFeedback(0)}>{text}</button>}
+      {text === "neutral" && <button onClick={() => handleFeedback(1)}>{text}</button>}      
+      {text === "bad" && <button onClick={() => handleFeedback(2)}>{text}</button>}
+    </div>
+  );
+};
+
+function App() {
+
+  const [counts, setCounts] = useState([0, 0, 0]);
+  const [isVisible, setIsVisible] = useState(false);
+
+  
+
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>give feedback</h1>
-        <button onClick={() => handleFeedback(0)}>good</button>
-        <button onClick={() => handleFeedback(1)}>neutral</button>
-        <button onClick={() => handleFeedback(2)}>bad</button>
+        <Button counts={counts} setCounts={setCounts} setIsVisible={setIsVisible} text={"good"}></Button>
+        <Button counts={counts} setCounts={setCounts} setIsVisible={setIsVisible} text={"neutral"}></Button>
+        <Button counts={counts} setCounts={setCounts} setIsVisible={setIsVisible} text={"bad"}></Button>
       </header>
       <body className="App-body">
         <h2>statistics</h2>
         {!isVisible && (<h3>No hay ingresos aún</h3>)}
         {isVisible && (
         <div className='estadistica' >
-        <ul>good {counts[0]}</ul>
-        <ul>neutral {counts[1]}</ul>
-        <ul>bad {counts[2]}</ul>
-        <ul>total {counts[0]+counts[1]+counts[2]}</ul>
-        <Statistics counts={counts}></Statistics>
+        <table>
+          <tr>
+            <td><Statistics counts={counts} estadistica={"good"} nombre_valor={"nombre"}></Statistics></td>
+            <td><Statistics counts={counts} estadistica={"good"} nombre_valor={"valor"}></Statistics></td>
+          </tr>
+          <tr>
+            <td><Statistics counts={counts} estadistica={"neutral"} nombre_valor={"nombre"}></Statistics></td>
+            <td><Statistics counts={counts} estadistica={"neutral"} nombre_valor={"valor"}></Statistics></td>
+          </tr>
+          <tr>
+            <td><Statistics counts={counts} estadistica={"bad"} nombre_valor={"nombre"}></Statistics></td>
+            <td><Statistics counts={counts} estadistica={"bad"} nombre_valor={"valor"}></Statistics></td>
+          </tr>
+          <tr>
+            <td><Statistics counts={counts} estadistica={"total"} nombre_valor={"nombre"}></Statistics></td>
+            <td><Statistics counts={counts} estadistica={"total"} nombre_valor={"valor"}></Statistics></td>
+          </tr>
+          <tr>
+            <td><Statistics counts={counts} estadistica={"average"} nombre_valor={"nombre"}></Statistics></td>
+            <td><Statistics counts={counts} estadistica={"average"} nombre_valor={"valor"}></Statistics></td>
+          </tr>
+          <tr>
+            <td><Statistics counts={counts} estadistica={"positive"} nombre_valor={"nombre"}></Statistics></td>
+            <td><Statistics counts={counts} estadistica={"positive"} nombre_valor={"valor"}></Statistics></td>
+          </tr>
+        </table>
         </div>)}
       </body>
     </div>
